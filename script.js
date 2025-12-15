1882571526618a5f35478452f29690069f320dc8
Max Dendy <80255339+MaxKamenskii@users.noreply.github.com>
2025-12-15 22:23:37 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("This is working");
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    let newNode = new Node(value);
+    if (this.head == null) {
+      this.head = newNode;
+    } else {
+      let nextOne = this.head;
+      while (nextOne.nextNode != null) {
+        nextOne = nextOne.nextNode;
+      }
+      nextOne.nextNode = newNode;
+    }
+  }
+
+  prepend(value) {
+    let newNode = new Node(value);
+    if (this.head != null) {
+      let copyNode = this.head;
+      this.head = newNode;
+      this.head.nextNode = copyNode;
+    }
+  }
+}
+
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+const myList = new LinkedList();
+
+console.log(myList);
+
+myList.append("Anode");
+myList.append("Bnode");
+myList.append("Cnode");
+myList.append("Dnode");
+myList.prepend("Pnode");
+
+console.log(myList);
